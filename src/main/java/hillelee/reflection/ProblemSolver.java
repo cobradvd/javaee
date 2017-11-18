5013df2750a8a0a982c590dc618795eb72edaf7f
max <farsikovmax@gmail.com>
2017-11-18 10:44:24 +0200

    home work

--- src/main/java/hillelee/reflection/ProblemSolver.java
+++ src/main/java/hillelee/reflection/ProblemSolver.java
@@ -1,6 +1,10 @@
 package hillelee.reflection;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 import lombok.SneakyThrows;
 
@@ -8,15 +12,24 @@ import lombok.SneakyThrows;
  * Created by User on 28.10.2017.
  */
 public class ProblemSolver {
-    @SneakyThrows
+    //@SneakyThrows
     public String solve(Object problem) {
-        Class<?> aClass = problem.getClass();
-        Method[] methods = aClass.getMethods();
-        for (Method method : methods) {
-            if (method.isAnnotationPresent(CorrectAnswer.class)) {
-                return (String) method.invoke(problem);
+        return Stream.of(problem)
+                     .map(Object::getClass)
+                     .flatMap(clazz -> Arrays.stream(clazz.getMethods()))
+                     .filter(method -> method.isAnnotationPresent(CorrectAnswer.class))
+                     .map(invokeOn(problem))
+                     .findFirst()
+                     .orElseThrow(() -> new RuntimeException("There is no CorrectAnswer annotation"));
+    }
+
+    private Function<Method, String > invokeOn(Object object){
+        return method -> {
+            try {
+                return (String) method.invoke(object);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
-        }
-        throw new RuntimeException("There is no CorrectAnswer annotation");
+        };
     }
 }
